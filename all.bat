baf1fcc8acdeddf289d2967c3eb3c5b2b0cde4e4
Paul Draper <paulddraper@gmail.com>
2013-04-19 10:26:02 -0600

    Add batch file for generating reports

---
+++ all.bat
@@ -0,0 +1,5 @@
+C:\Python27\python main.py > out.txt
+C:\Python27\python -m inet_sim.plot.sequence -i out.txt -o sequence.png
+C:\Python27\python -m inet_sim.plot.queue -i out.txt -o queue.png
+C:\Python27\python -m inet_sim.plot.window -i out.txt -o window.png
+C:\Python27\python -m inet_sim.plot.rate -i out.txt -o rate.png
